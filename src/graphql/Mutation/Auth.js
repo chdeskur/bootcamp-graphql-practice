bec9e8a32e5d6aebf2ad7a4cdc3e94c4fd5387fb
chdeskur <chdeskur@gmail.com>
2021-01-20 14:18:26 -0500

    addAuth

---
+++ src/graphql/Mutation/Auth.js
@@ -0,0 +1,36 @@
+const { UserInputError } = require('apollo-server-express')
+const User = require('../../models/User')
+const {
+  hashPassword, comparePassword, createToken,
+} = require('../../lib/auth')
+
+
+const login = async (obj, { email, password }) => {
+  const user = await User.query().findOne({
+    email,
+  })
+  if (!user) {
+    throw new UserInputError('Invalid email or password')
+  }
+
+  const validPassword = await comparePassword(password, user.password)
+  if (!validPassword) {
+    throw new UserInputError('Invalid email or password')
+  }
+
+
+  // If successful login, set authentication information
+  const payload = {
+    id: user.id,
+  }
+  const token = createToken(payload)
+
+  return { user, token }
+}
+
+
+const resolver = {
+  Mutation: { login },
+}
+
+module.exports = resolver
--- src/graphql/Mutation/index.js
+++ src/graphql/Mutation/index.js
@@ -1,7 +1,8 @@
 const merge = require('lodash.merge')
 const Author = require('./Author')
 const Book = require('./Book')
+const Auth = require('./Auth')
 
-const resolvers = [Author, Book]
+const resolvers = [Author, Book, Auth]
 
 module.exports = merge(...resolvers)
